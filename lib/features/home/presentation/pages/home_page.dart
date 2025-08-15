d019373daa229497af6a0e29918f929f8c085ef2
Ron Tzarfati <rontzarfati@example.com>
2025-08-16 01:09:28 +0300

    Claude auto update on

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -7,6 +7,7 @@ import '../../../../shared/widgets/animated_gradient_background.dart';
 import '../../../../shared/widgets/neon_text.dart';
 import '../../../gallery/presentation/pages/gallery_page.dart';
 import '../../../tutorials/presentation/pages/tutorials_page.dart';
+import '../../../updates/presentation/pages/updates_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
