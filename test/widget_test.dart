a6b24091cd3f876b99d650ba2cf30cea2a7b0ff2
Ron Tzarfati <rontzarfati@example.com>
2025-08-16 03:16:02 +0300

    Claude auto update on

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,5 @@
 // Basic widget test for Zaza Dance app
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
