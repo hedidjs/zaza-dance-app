745ad248ed38ae24f185549e3856fbf8dd9e2c01
Ron Tzarfati <rontzarfati@example.com>
2025-08-16 15:10:05 +0300

    Claude auto update on

--- lib/features/settings/presentation/pages/profile_settings_page.dart
+++ lib/features/settings/presentation/pages/profile_settings_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../../../core/constants/app_colors.dart';
 import '../../../../core/providers/auth_provider.dart';
