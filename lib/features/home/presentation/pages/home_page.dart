998c1ce3b9db38004d3fc302e7bcfcb8d570b441
Ron Tzarfati <rontzarfati@example.com>
2025-08-16 03:55:50 +0300

    Claude auto update on

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -6,6 +6,7 @@ import '../../../../core/constants/app_colors.dart';
 import '../../../../shared/widgets/animated_gradient_background.dart';
 import '../../../../shared/widgets/neon_text.dart';
 import '../../../../shared/widgets/neon_hebrew_text.dart';
+import '../../../../shared/widgets/app_drawer.dart';
 import '../../../gallery/presentation/pages/gallery_page.dart';
 import '../../../tutorials/presentation/pages/tutorials_page.dart';
 import '../../../updates/presentation/pages/updates_page.dart';
